437787e53ced6b2bc897b1c17d7a9c3cc1c09808
Phuc185 <danhuynh98@gmail.com>
2020-07-13 07:48:27 +0700

    cat layout

---
+++ resources/views/admin/to_chuc.blade.php
@@ -0,0 +1,39 @@
+@extends('admin_layout')
+@section('admin_content')
+<div class="table-agile-info">
+    <div class="panel panel-default">
+        <?php
+        foreach($tochuc as $key => $tc){
+        $tc_ten = $tc->tc_ten;
+        $tc_tructhuoc = $tc->tc_tructhuoc;
+        }
+        ?>
+        <div class="panel-heading">
+            <h4 style="text-align: center; color: #fe980f ">Thông tin Tổ chức</h4>
+        </div>
+        <div class="table-responsive">
+        <?php 
+        $message = Session::get('message');
+        if($message){
+            echo '<span class="text-alert">'.$message.'</span>';
+            Session::put('message',null);
+        }
+        ?>
+        <table class="table table-striped b-t b-light">
+          <thead>
+            <tr>
+              <th>Tên tổ chức</th>
+              <th>Trực thuộc</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+            <td>{{$tc_ten}}</td>
+            <td>{{$tc_tructhuoc}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+	</div>
+</div>
+@endsection
