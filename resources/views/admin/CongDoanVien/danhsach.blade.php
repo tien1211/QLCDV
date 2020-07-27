f7e16b063307a48b066d60ab8508492cf7982029
unknown <hatien.1211@gmail.com>
2020-07-27 09:13:34 +0700

    addd

--- resources/views/admin/CongDoanVien/danhsach.blade.php
+++ resources/views/admin/CongDoanVien/danhsach.blade.php
@@ -177,17 +177,7 @@
 
 
 
-  function loadAdmin() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", "{{route('CDV_DS')}}", true);
-    xhttp.send();
-  }
+
   
   </script>
 @endsection
