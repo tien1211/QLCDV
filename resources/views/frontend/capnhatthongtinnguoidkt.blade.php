74a667bab85b82339f7b8bd1616e14f3b7428c68
unknown <hatien.1211@gmail.com>
2020-08-06 10:57:20 +0700

    messgaeê

--- resources/views/frontend/capnhatthongtinnguoidkt.blade.php
+++ resources/views/frontend/capnhatthongtinnguoidkt.blade.php
@@ -12,17 +12,17 @@
     {{ csrf_field() }}
     <input type="hidden" name="dkt_soluong" value="{{$soluong}}">
 @for($i = 0 ; $i < $soluong; $i++)
-<div class="row">
-    <div class="col-12">
+{{-- <div class="row ">
+    <div class="col-12"> --}}
         <!-- Form -->
         <div class="roberto-contact-form">
                 <div class="row">
-                    <div class="col-12 col-lg-9 wow fadeInUp" data-wow-delay="100ms">
+                    <div class="col-lg-9 wow fadeInUp" data-wow-delay="100ms">
                         <input type="text" required name="ttndk_ten[{{$i}}]"  class="form-control mb-30" placeholder="Tên người tham gia thứ {{$i + 1}}">
                     </div>
-                    <div class="col-12 col-lg-3 wow fadeInUp" data-wow-delay="100ms">
-                        <select class="form-control" name="ttndk_gt[{{$i}}]" required>
-                        <option value="">Chọn giới tính...</option>
+                    <div class="col-lg-3 wow fadeInUp"  data-wow-delay="100ms">
+                        <select class="form-control" name="ttndk_gt[{{$i}}]" required >
+                        <option value="">Giới tính</option>
                         <option value="1">Nam</option>
                         <option value="0">Nữ</option>
                         </select>
--- resources/views/frontend/quanlytour.blade.php
+++ resources/views/frontend/quanlytour.blade.php
@@ -2,6 +2,7 @@
 @section('frontend_content')
 <div class="room-service mb-50">
     <h4>Danh Sách Tour đã đăng ký</h4>
+    
     <table class="table">
         <thead class="thead-light">
             <tr>
@@ -9,7 +10,7 @@
                 <th>Tên tour</th>
                 <th>Giai đoạn</th>
                 <th>Chi phí</th>
-                <th>Người tham gia</th>
+                <th>Thao Tác</th>
             </tr>
         </thead>
         <tbody>
@@ -19,7 +20,9 @@
             <td>{{$tour->lt_ten}} {{date('Y ',strtotime($tour->tour_handk))}}</td>
             <td>{{$tour->giai_doan}}</td>
             <td>{{$tour->tour_chiphi}}</td>
-            <td><a href="{{route('DS_NTG',['id'=>$tour->tour_id])}}" class="post-author">Chi tiết</a></td>
+            <td><a href="{{route('DS_NTG',['id'=>$tour->tour_id])}}" class="post-author"><li class="fas fa-list" style="font-size: 20px"></li></a>&nbsp;&nbsp;&nbsp;
+                <a href="{{route('DS_NTG',['id'=>$tour->tour_id])}}" class="post-author"><li class="fas fa-trash-alt" style="font-size: 20px;"></li></a>
+            </td>
             </tr>
             @endforeach
         </tbody>
