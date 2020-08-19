a4f524508d0876f2bf0ce4dfcfad75c92b60bc99
Nguyennhuttruong <nntruongit1998>
2020-08-19 10:22:29 +0700

    hhhhh

--- resources/views/admin/CongDoanVien/imp.blade.php
+++ resources/views/admin/CongDoanVien/imp.blade.php
@@ -14,25 +14,25 @@
                 </span>
             </header>
             <div class="panel-body">
-                
+
                 <form method="post" action="{{route('Import')}}" enctype="multipart/form-data">
                     @csrf
 
                     <div class="form-group ">
                         <a href="samp/Samp.xlsx"><label for="username" class="control-label col-lg-3">DOWNLOAD MẪU EXCEL</label></a>
                     </div>
-                    
+
                     <div class="form-group ">
                         <label for="username" class="control-label col-lg-12">Chèn File:</label>
-                        
+
                         <div class="col-sm-6 col-lg-12">
                             <input class="form-control "  name="file" type="file">
-                            @if($errors->has('file')) 
+                            @if($errors->has('file'))
                             <div style="color:red">{{ $errors->first('file')}}</div>
                             @endif
                         </div>
                     </div>
-                
+
                 </form>
             </div>
         </section>
@@ -61,18 +61,18 @@
                           </div>
                           </div>
                         <div class="form-group ">
-                            
-                           
+
+
                             <label for="username" class="control-label col-lg-3">FILE IMPORT</label>
                                 <div class="col-lg-6">
                                     <input class="form-control" name="file" type="file">
-                                    
+
                 {{------------------- SHOW ERROR ------------------}}
                                     @if($errors->has('file'))
                                     <div class="alert alert-warning" style="color: red">
                                         <ul>ERROR FILE IMPORT:</ul>
-                                            <li style="color: red">{{ $errors->first('file')}}</li> 
-                                        
+                                            <li style="color: red">{{ $errors->first('file')}}</li>
+
                                     </div>
                                    @endif
                                     @if (isset($f))
@@ -80,47 +80,37 @@
                                             @foreach ($f as $f1)
                                                 <ul>
                                                     @foreach ($f1->errors() as $error)
-                                                   
+
                                                     <li style="color: red; margin-left:5%">{{ $error }}</li>
-                                                
+
                                                     @endforeach
-                                                    
+
                                                 </ul>
                                                 @break
                                             @endforeach
                                         </div>
                                     @endif
                                 </div>
-                {{------------------- SHOW ERROR ------------------}}           
-                        
-            
+                {{------------------- SHOW ERROR ------------------}}
+
+
             </div>
-            
+
                         <div class="form-group">
-                            <div class="col-lg-offset-3 col-lg-6">
-                                <button class="btn btn-primary" type="submit">SUBMIT</button>
+                            {{-- <div class="col-lg-offset-3 col-lg-6"> --}}
+                                <button class="btn btn-primary" style="margin-left:332px " type="submit">SUBMIT</button>
                                 <a href="{{route('LT_DanhSach')}}"><button class="btn btn-default" type="button">Thoát</button></a>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-lg-12">
-                            <label for="username" class="control-label col-lg-3">DOWNLOAD FILE EXCEL:</label>
-                                <div class="col-lg-2">
-                                    <a href="samp/FormImport.xlsx"><label for="username" class="control-label col-lg-3"> TẠI ĐÂY </label></a>
-                                </div>
+                            <div class="form-group">
+                            <label for="username" style="margin-left:205px ">DOWNLOAD FILE EXCEL:</label>
+                            <a href="samp/FormImport.xlsx"><label for="username" > TẠI ĐÂY </label></a>
+                            </div>
+                            <div class="form-group">
+                            <label for="username"  style="margin-left:205px ">DOWNLOAD FILE MẪU:</label>
+                            <a href="samp/ExampleForm.xlsx" ><label for="username" >TẠI ĐÂY </label></a>
                             </div>
                         </div>
 
-                        <div class="form-group">  
-                            <div class="col-lg-12">
-                                <label for="username" class="control-label col-lg-3" >DOWNLOAD FILE MẪU:</label>
-                                <div class="col-lg-2">
-                                
-                                    <a href="samp/ExampleForm.xlsx" ><label for="username" class="control-label col-lg-6">TẠI ĐÂY </label></a>
-
-                                </div>
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
