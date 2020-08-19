fed58442dafca00acfbe9256f3f0be30fe17e866
unknown <hatien.1211@gmail.com>
2020-08-19 09:53:29 +0700

    1908

--- app/Imports/CongDoanVienImport.php
+++ app/Imports/CongDoanVienImport.php
@@ -22,6 +22,28 @@ class CongDoanVienImport implements ToCollection
             if($key>0){
                 
                 DB::table('CongDoanVien')->insert([
+                    // 'dv_id'             => $value[0],
+                    // 'cv_id'             => $value[1],
+                    // 'lnv_id'            => $value[2],
+                    // 'cdv_ten'           => $value[3],
+                    // 'cdv_ngaysinh'      => Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($value[4]))->format('Y-m-d'),
+                    // 'cdv_gioitinh'      => $value[5],#1 là nam 0 là nữ
+                    // 'cdv_cmnd'          => $value[6],
+                    // 'cdv_nguyenquan'    => $value[7],
+                    // 'cdv_diachi'        => $value[8],
+                    // 'cdv_sdt'           => $value[9],
+                    // 'cdv_email'         => $value[10],
+                    // 'cdv_dantoc'        => $value[11],
+                    // 'cdv_trinhdo'       => $value[12],
+                    // 'cdv_tongiao'       => $value[13],
+                    // 'cdv_ngaythuviec'   => Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($value[14]))->format('Y-m-d'),
+                    // 'cdv_ngayvaonganh'  => Carbon\Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($value[15]))->format('Y-m-d'),
+                    // 'cdv_username'      => $value[16],
+                    // 'password'          => bcrypt($value[17]),
+                    // 'cdv_quyen'         => 0,
+                    // 'cdv_trangthai'     => 1,
+                    // 'mht_id'            => 1,
+
                     'dv_id' => $value[0],
                     'cv_id' => $value[1],
                     'lnv_id' => $value[2],
@@ -43,6 +65,7 @@ class CongDoanVienImport implements ToCollection
                     'cdv_username' => $value[16],
                     'password' => bcrypt($value[17]),
                     'cdv_quyen' => 0
+
                 ]);
             }
         }
@@ -50,40 +73,89 @@ class CongDoanVienImport implements ToCollection
 
     public function rules(): array{
         return [
+            // '0'       =>  'required',
+            // '1'       =>  'required',
+            // '2'       =>  'required',
+            // '3'       =>  'required',
+            // '4'       =>  'required',
+            // '5'       =>  'required',
+            // '6'       =>  'required',
+            // '7'       =>  'required',
+            // '8'       =>  'required',
+            // '9'       =>   'required',
+            // '10'      =>  ['required',Rule::unique('CongDoanVien', 'cdv_email')],
+            // '11'      =>  'required',
+            // '12'      =>  'required',
+            // '13'      =>  'required',
+            // '14'      =>  'required',
+            // '15'      =>  'required',
+            // '16'      =>  ['required',Rule::unique('CongDoanVien', 'cdv_username')],
+            // '17'      =>  'required'
+
             '0'       =>  'required',
             '1'       =>  'required',
             '2'       =>  'required',
-            '3'       =>  'required',
             '4'       =>  'required',
             '5'       =>  'required',
             '6'       =>  'required',
+            // '7'       =>  ['required','min:9','max:9'],
             '7'       =>  'required',
             '8'       =>  'required',
-            '9'       =>   'required',
-            '10'      =>  ['required',Rule::unique('CongDoanVien', 'cdv_email')],
-            '11'      =>  'required',
+            '9'       =>  'required',
+            // '10'      =>  ['required','min:9','max:9'],
+            '10'      =>  'required',
+            '11'      =>  ['required', Rule::unique('CongDoanVien', 'cdv_email')],
             '12'      =>  'required',
             '13'      =>  'required',
             '14'      =>  'required',
             '15'      =>  'required',
-            '16'      =>  ['required',Rule::unique('CongDoanVien', 'cdv_username')],
-            '17'      =>  'required'
+            '16'      =>  'required',
+            '18'      =>  ['required',Rule::unique('CongDoanVien', 'cdv_email')],
+            // '19'      =>  ['required','min:8','max:50'],
+            '19'      =>  'required',
+            // '20'      =>  'required',
+            
         ];
     }
 
 
     public function customValidationMessages(){
         return [
+                // '0.required'    => trans('Không được để trống đơn vị'),
+                // '1.required'    => trans('Không được để trống chức vụ'),
+                // '2.required'    => trans('Không được để trống loại nhân viên'),
+                // '4.required'    => trans('Không được để trống tên công đoàn viên'),
+                // '5.required'    => trans('Không được để trống ngày sinh'),
+                // '6.required'    => trans('Không được để trống giới tính'),
+                // '7.required'    => trans('Không được để trống CMND'),
+                // '8.required'    => trans('Không được để trống nguyên quán'),
+                // '9.required'    => trans('Không được để trống địa chỉ'),
+                // '10.required'   => trans('Không được để trống số điện thoại'),
+                // '11.required'   => 'Không được để trống email',
+                // '11.unique'     => 'Email đã được tồn tại vui lòng nhập lại email',
+                // '12.required'   => trans('Không được để trống dân tộc'),
+                // '13.required'   => trans('Không được để trống trình độ'),
+                // '14.required'   => trans('Không được để trống tôn giáo'),
+                // '15.required'   => trans('Không được để trống ngày thử việc'),
+                // '16.required'   => trans('Không được để trống ngày vào ngành'),
+                // '17.required'   => trans('Không được để trống tên đăng nhập'),
+
+
                 '0.required'    => trans('Không được để trống đơn vị'),
                 '1.required'    => trans('Không được để trống chức vụ'),
                 '2.required'    => trans('Không được để trống loại nhân viên'),
                 '4.required'    => trans('Không được để trống tên công đoàn viên'),
                 '5.required'    => trans('Không được để trống ngày sinh'),
+                // '5.date'        => trans('Vui lòng nhập đúng định dạng ngày sinh'),
                 '6.required'    => trans('Không được để trống giới tính'),
                 '7.required'    => trans('Không được để trống CMND'),
+                // '7.min'         => trans('Vui lòng nhập đủ 9 kí tự cmnd'),
+                // '7.max'         => trans('Vui lòng nhập đủ 9 kí tự cmnd'),
                 '8.required'    => trans('Không được để trống nguyên quán'),
                 '9.required'    => trans('Không được để trống địa chỉ'),
                 '10.required'   => trans('Không được để trống số điện thoại'),
+                // '10.min'        => trans('Vui lòng nhập đủ 9 kí tự sdt'),
+                // '10.max'        => trans('Vui lòng nhập đủ 9 kí tự sdt'),
                 '11.required'   => 'Không được để trống email',
                 '11.unique'     => 'Email đã được tồn tại vui lòng nhập lại email',
                 '12.required'   => trans('Không được để trống dân tộc'),
@@ -91,7 +163,12 @@ class CongDoanVienImport implements ToCollection
                 '14.required'   => trans('Không được để trống tôn giáo'),
                 '15.required'   => trans('Không được để trống ngày thử việc'),
                 '16.required'   => trans('Không được để trống ngày vào ngành'),
-                '17.required'   => trans('Không được để trống tên đăng nhập'),
+                '18.required'   => trans('Không được để trống tên đăng nhập'),
+                '18.unique'     => trans('Tên đăng nhập đã được tồn tại'),
+                '19.required'   => trans('Vui lòng không được để trống mật khẩu'),
+                // '19.min'        => 'Mật khẩu phải ít nhất 8 kí tự',
+                // '19.max'        => 'Mật khẩu phải tối đa 50 kí tự',
+                // '20.required'   => trans('Vui lòng không được để trống quyền')
             ];
         }
 }
